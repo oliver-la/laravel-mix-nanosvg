ed9fa647e720387e5dd2956d4298c9637b219919
ola <oliver.lazovic@mindstudios.ch>
2022-04-07 16:47:43 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const mix = require('laravel-mix');
+const path = require('path');
+const ManifestPlugin = require('./manifest-plugin.js');
+
+class NanoSvg {
+    name() {
+        return ['nanosvg', 'svg'];
+    }
+
+    dependencies() {
+        return ['nanosvg'];
+    }
+
+    register(pattern, destination = '', options = {}) {
+        this.patterns = [].concat(patterns);
+        const nanofy = require("nanosvg");
+
+        await nanofy(
+            pattern,
+            path.join(Mix.config.publicPath, destination),
+            options
+        )
+    }
+
+    webpackPlugins() {
+        let {patterns} = this;
+
+        return [
+            new ManifestPlugin(patterns),
+        ];
+    }
+}
+
+mix.extend('nanosvg', new NanoSvg());
